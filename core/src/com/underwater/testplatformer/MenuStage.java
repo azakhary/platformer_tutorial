32671ffdba88ce45bb9d0febffaf93bf7566da2c
Avetis Zakharyan <avetis.zakharyan@gmail.com>
2014-11-09 12:49:44 +0400

    Stage isback

--- core/src/com/underwater/testplatformer/MenuStage.java
+++ core/src/com/underwater/testplatformer/MenuStage.java
@@ -1,8 +1,5 @@
 package com.underwater.testplatformer;
 
-import com.badlogic.gdx.InputAdapter;
-import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
@@ -12,15 +9,16 @@ import com.uwsoft.editor.renderer.script.SimpleButtonScript;
 
 public class MenuStage extends Stage {
 
-    private SceneLoader sl;
-
     public MenuStage(final GameStage gameStage) {
 
         //Creatinga a scene loader and passing it a resource manager of game stage
-        sl = new SceneLoader(gameStage.essentials.rm);
+        SceneLoader sl = new SceneLoader(gameStage.essentials.rm);
 
         // loading UI scene
         sl.loadScene("UIScene");
+
+        // adding it's root composite item to the stage
+        addActor(sl.getRoot());
 
         // Creating restart button, and adding a click listener to it
         SimpleButtonScript restartButton = SimpleButtonScript.selfInit(sl.getRoot().getCompositeById("restartBtn"));
